69a8c9c6c0c801b0ed952d5baa7a9d2c66be4e31
dickanirwansyah <dickanirwansyah@gmail.com>
2018-01-29 17:13:28 +0700

    client

---
+++ oauth2-resource-client/src/main/java/com/dicka/oauth2/oauth2resourceclient/Oauth2ResourceClientApplication.java
@@ -0,0 +1,12 @@
+package com.dicka.oauth2.oauth2resourceclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ResourceClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2ResourceClientApplication.class, args);
+	}
+}
